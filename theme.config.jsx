5f94c859c54aa3e6321a5447deee392ea79d5a3c
G. A. 02 <blessedbyhope05@gmail.com>
2024-03-29 15:47:35 +0700

    commit

---
+++ theme.config.jsx
@@ -0,0 +1,31 @@
+export default {
+      logo: <span>Introduction to Operating System | Full Course for Beginners - Mike Murphy - V2 - made with Lumentis</span>,
+      editLink: {
+        component: null,
+      },
+      project: {
+        link: "https://github.com/hrishioa/lumentis",
+      },
+      feedback: {
+        content: null,
+      },
+      footer: {
+        text: (
+          <>
+            Made with 🫶 by&nbsp;
+            <a href="https://twitter.com/hrishioa" target="_blank">
+              Hrishi - say hi!
+            </a>
+          </>
+        ),
+      },
+      head: (
+        <>
+          <meta property="og:title" content="Introduction to Operating System | Full Course for Beginners - Mike Murphy - V2" />
+          <meta property="og:description" content="The PrimarySource appears to be a transcript of a series of video lectures about operating systems. The lectures cover a wide range of topics related to operating system concepts, including hardware resources, disk input/output, memory management, processes, and more. The target audience for these lectures is likely students or professionals in computer science or a related field who want to learn about operating system fundamentals and implementation details." />
+          <meta name="robots" content="noindex, nofollow" />
+          <link rel="icon" type="image/x-icon" href="https://raw.githubusercontent.com/HebeHH/lumentis/choose-favicon/assets/default-favicon.png" />
+        </>
+      ),
+    };
+    
